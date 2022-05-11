d151ed728564f8d714a0a319d2edc9c73d6c326f
Marissa Flynn <mkflynn13@gmail.com>
2022-05-10 20:11:23 -0400

    Added some dependency info in the config and package.json.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,7 +17,38 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
+    plugins: [ 
+
+      // Webpack plugin that generates our html file and injects our bundles.
+      new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Text Editor'
+    }),
+
+    // Injects the custom service worker
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'src-sw.js',
+    }),
+
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'Text Editor',
+      short_name: 'TextEdit',
+      description: 'Just another text editor',
+      background_color: '#225ca3',
+      theme_color: '#225ca3',
+      start_url: '/',
+      publicPath: '/',
+      icons: [
+        {
+          src: path.resolve('./src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
       
     ],
 
@@ -26,7 +57,19 @@ module.exports = () => {
         {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
-        }
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // Use babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
